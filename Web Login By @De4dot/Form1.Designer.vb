93efe286605391e1791a85373751331e677204dd
Ahmed Al'Jabari <42383132+De4d0t@users.noreply.github.com>
2020-05-17 20:40:58 +0300

    Add files via upload

---
+++ Web Login By @De4dot/Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(17, 69)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(292, 25)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Login"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(17, 17)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(292, 20)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = "De4dot"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(17, 43)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(292, 20)
+        Me.TextBox2.TabIndex = 2
+        Me.TextBox2.Text = "Password"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.TextBox2.UseSystemPasswordChar = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(102, 97)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(118, 13)
+        Me.LinkLabel1.TabIndex = 3
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "Follow Us for new tools"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(321, 119)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Winform Login By @De4dot"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents LinkLabel1 As LinkLabel
+End Class
---
+++ Web Login By @De4dot/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Text
+'Coded and extracted by Ahmed Al-Jabari 
+'Process.Start("https://t.me/YYxYY")
+'Process.Start("https://www.instagram.com/de4dot")
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+
+            Dim Bytes As Byte() = Encoding.Default.GetBytes("username=" & TextBox1.Text & "&enc_password=#PWD_INSTAGRAM_BROWSER:0:1589682409:" & TextBox2.Text & "&queryParams={}&optIntoOneTap=false")
+
+            Dim Request_Login As HttpWebRequest = DirectCast(WebRequest.Create("https://www.instagram.com/accounts/login/ajax/"), HttpWebRequest)
+
+            With Request_Login
+                .Method = "POST"
+                .Host = "www.instagram.com"
+                .KeepAlive = True
+                .UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.73 Safari/537.36"
+                .Accept = "*/*"
+                .Referer = "https://www.instagram.com/de4dot"
+                .ContentType = "application/x-www-form-urlencoded"
+                .Headers.Add("X-Requested-With", "XMLHttpRequest")
+                .Headers.Add("X-IG-App-ID", "936619743392459")
+                .Headers.Add("X-Instagram-AJAX", "AJ")
+                .Headers.Add("X-CSRFToken", "AJ")
+                .Headers.Add("Origin", "https://www.instagram.com")
+                .Headers.Add("Sec-Fetch-Site", "same-origin")
+                .Headers.Add("Accept-Language", "en-US,en;q=0.9")
+                .AutomaticDecompression = DecompressionMethods.Deflate Or DecompressionMethods.GZip
+                .ContentLength = Bytes.Length
+            End With
+
+            Dim Stream As Stream = Request_Login.GetRequestStream()
+            Stream.Write(Bytes, 0, Bytes.Length)
+            Dim Responseee As HttpWebResponse = Request_Login.GetResponse
+            Dim Reader As New StreamReader(Responseee.GetResponseStream())
+            Dim AhmedAljabariS As String = Reader.ReadToEnd
+
+            MsgBox(AhmedAljabariS)
+
+        Catch ex As WebException
+            Dim uu As String = New StreamReader(ex.Response.GetResponseStream()).ReadToEnd()
+            MsgBox("Error : " & uu)
+        End Try
+    End Sub
+    Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
+        Process.Start("https://t.me/YYxYY")
+        Process.Start("https://www.instagram.com/de4dot")
+        Process.Start("https://www.youtube.com/de4dot")
+    End Sub
+End Class
